a99ac60badf8771c69ef0b524d45cd9629397d38
WillChew <williamchew95@gmail.com>
2018-08-27 19:17:35 -0400

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Will Chew on 2018-08-27.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
